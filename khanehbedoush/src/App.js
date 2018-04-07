d3a28cea3053c061b65ac8e71a6cf7cad5f8e6b7
mhyousefi <mhyousefi96@gmail.com>
2018-04-07 12:34:22 +0430

    Enabled redirecting to AddCreditPage when clicking on the button in ProfileAreaDropDown

--- khanehbedoush/src/App.js
+++ khanehbedoush/src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom'
 import AddCreditPage from 'src/components/pages/AddCreditPage'
 import HomePage from 'src/components/pages/HomePage'
 import SearchResultsPage from 'src/components/pages/SearchResultsPage'
-import 'src/styles/General.css'
 
 
 export default class App extends Component {
--- khanehbedoush/src/components/general/Layout/Header/ProfileAreaDropDown.js
+++ khanehbedoush/src/components/general/Layout/Header/ProfileAreaDropDown.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import Fa from 'src/constants/Fa'
 import 'src/styles/Header/Header.css'
 import 'src/styles/UserInfoCard/UserInfoCard.css'
 
 
 export default class ProfileAreaDropDown extends Component {
+  state = {
+    redirectToAddCredit: false
+  }
+
+  _handleButtonClick = () => {
+    this.setState({ redirectToAddCredit: true });
+  }
+
   render() {
+    if (this.state.redirectToAddCredit) {
+      return <Redirect to='/AddCredit'/>
+    }
+
     return (
       <div className="profileAreaDropDownContent curvedCorner" id={this.props.isHomePage ? "homePageProfileArea" : ""}>
         <div className="text userInfoCardUpperRow userInfoCardCommonRowProps">
@@ -16,7 +29,7 @@ export default class ProfileAreaDropDown extends Component {
           <div className="text">{Fa['2000 Toumans']}</div>
         </div>
         <div className="userInfoCardLowerRow">
-          <button className="userInfoCardBtn text curvedCorner">
+          <button className="userInfoCardBtn text curvedCorner" onClick={() => {this._handleButtonClick()}}>
             {Fa['increase credit']}
           </button>
         </div>
@@ -24,7 +37,3 @@ export default class ProfileAreaDropDown extends Component {
     )
   }
 }
-
-// ProfileAreaDropDown.propTypes = {
-//   isHomePage: PropTypes.bool
-// };
