62a23f203713034a5e71a8daf6c855585263246d
mhyousefi <mhyousefi96@gmail.com>
2018-04-07 12:43:01 +0430

    Enabled redirecting to SearchResultsPage when clicking on SearchForm button

--- khanehbedoush/src/components/general/SearchForm/SearchFormLower.js
+++ khanehbedoush/src/components/general/SearchForm/SearchFormLower.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import Fa from 'src/constants/Fa'
 import 'src/styles/HomePage/HomePageHeader.css'
 import 'src/styles/HomePage/HomePageResponsive.css'
@@ -7,7 +8,19 @@ import 'src/styles/SearchForm/SearchFormMediaQuery.css'
 
 
 export default class SearchFormLower extends Component {
+  state = {
+    redirectToSearchResults: false
+  }
+
+  _handleButtonClick = () => {
+    this.setState({ redirectToSearchResults: true })
+  }
+
   render() {
+    if (this.state.redirectToSearchResults) {
+      return <Redirect to="/SearchResults"/>
+    }
+
     return (
       <div className="searchFormLower">
         <div className="searchFormRadioContainer">
@@ -21,7 +34,7 @@ export default class SearchFormLower extends Component {
           </div>
         </div>
 
-        <button className="searchFormBtn curvedCorner searchFormBtnTxt">
+        <button className="searchFormBtn curvedCorner searchFormBtnTxt" onClick={this._handleButtonClick}>
           {Fa["search"]}
         </button>
       </div>
