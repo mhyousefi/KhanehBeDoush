411a0a385caea8c295a7a17e2cc2649feb6b6933
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2018-04-10 19:47:15 +0430

    WIP in developing API fucntions

--- khanehbedoush/src/api/BankApi.js
+++ khanehbedoush/src/api/BankApi.js
@@ -1,5 +1,22 @@
 export const changeCredit = (amount) => {
-  return true
+	fetch('localhost:8080/increaseCredit', {
+	 	method: 'POST',
+	  headers: {
+	    'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify({
+    		credit: amount
+		})
+	})
+	fetch('localhost:8080/increaseCredit')
+	  .then(checkStatus)
+	  .then(parseJSON)
+	  .then(function(data) {
+	  	console.log('parsed json field', data.response)
+	  	return (data.response  === "true")
+	  }).catch(function(error) {
+	    return false
+	})
 }
 
 
--- khanehbedoush/src/api/PhoneNumPurchase.js
+++ khanehbedoush/src/api/PhoneNumPurchase.js
@@ -1,7 +1,40 @@
-export const payForPhoneNum = (phoneNumber) => {
-  return true
+export const payForPhoneNum = (houseId) => {
+	fetch('localhost:8080/showHousePhoneNumber', {
+	  method: 'POST',
+	  headers: {
+	    'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify({
+    		houseId : houseId
+		})
+	})
+	fetch('localhost:8080/showHousePhoneNumber')
+	  .then(checkStatus)
+	  .then(parseJSON)
+	  .then(function(data) {
+	  	console.log('parsed json', data)
+	  	return data
+	  }).catch(function(error) {
+	  	return false
+	})
 }
 
-export const hasPaidForPhoneNum = (phoneNumber) => {
-  return false
+export const hasPaidForPhoneNum = (houseId) => {
+	fetch('localhost:8080/hasPaidForPhoneNum', {
+	  method: 'GET',
+	  headers: {
+	    'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify({
+    		houseId : houseId
+		})
+	})
+	fetch('localhost:8080/hasPaidForPhoneNum')
+	  .then(checkStatus)
+	  .then(parseJSON)
+	  .then(function(data) {
+	  	return (data.result === "true")
+	  }).catch(function(error) {
+	  	return false
+	})
 }
