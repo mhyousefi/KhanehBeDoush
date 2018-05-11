02726ef1a720fad507992744c27ad63040c877b4
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2018-05-11 13:03:57 +0430

    current credit is now supporting token-based auth. it was forgotten :)

--- src/Controllers/CurrentCredit.java
+++ src/Controllers/CurrentCredit.java
@@ -1,32 +1,27 @@
 package Controllers;
 
 import DAO.DAOUtils;
-import DAO.UserDAO;
 import Entities.IndividualUser;
 import Utilities.HeaderUtilities;
 import org.json.JSONObject;
 
-import javax.naming.NamingException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 
 @WebServlet("/currentCredit")
 public class CurrentCredit extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        HeaderUtilities.setHttpServletResponseHeader(response);
         try {
-            IndividualUser user = UserDAO.getIndividualUserById("2");
-            JSONObject jsonResponse = new JSONObject();
-            if (user != null) {
-                jsonResponse.put("currentCredit", user.getBalance());
-            }
+            HeaderUtilities.setHttpServletResponseHeader(response);
+            IndividualUser user = (IndividualUser) request.getAttribute("user");
+            JSONObject jsonResponse = new JSONObject().put("authenticated", "true");
+            jsonResponse.put("currentCredit", user.getBalance());
             DAOUtils.sendResponse(response, jsonResponse);
-        } catch (NamingException | SQLException e) {
-            e.printStackTrace();
+        }catch (Exception e){
+            DAOUtils.sendResponse(response, new JSONObject().put("serverError", e.getMessage()));
         }
     }
 }
