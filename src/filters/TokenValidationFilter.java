234ab543cf132f4b9b3c0920fd594b5fffea82ac
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2018-05-11 11:26:00 +0430

    Adding first token based Auth to increaseCredit by adding a filter named TokenValidationFilter which is gonna handle the token based Auth process for any controller that needs it.

--- src/filters/TokenValidationFilter.java
+++ src/filters/TokenValidationFilter.java
@@ -1,13 +1,19 @@
 package filters;
 
+import DAO.DAOUtils;
+import Entities.IndividualUser;
+import Utilities.HeaderUtilities;
+import Utilities.JSONFunctions;
+import org.json.JSONObject;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(filterName = "filters.TokenValidationFilter", urlPatterns = {"/*"})
+import static Utilities.TokenUtilities.getUserIdIfTokenIsValid;
+
+@WebFilter(filterName = "filters.TokenValidationFilter", urlPatterns = {"/increaseCredit", "/hasPaidForPhoneNum", "/showHousePhoneNumber", "/getHomeById"})
 public class TokenValidationFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -15,19 +21,27 @@ public class TokenValidationFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-        HttpSession session = request.getSession(false);
-        String loginURI = request.getContextPath() + "/login";
-
-        boolean loggedIn = session != null && session.getAttribute("user") != null;
-        boolean loginRequest = request.getRequestURI().equals(loginURI);
-
-        if (loggedIn || loginRequest) {
-            chain.doFilter(request, response);
-        } else {
-            response.sendRedirect(loginURI);
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HeaderUtilities.setHttpServletResponseHeader(response);
+        JSONObject requestInJson = JSONFunctions.createJSONObjectFromRequest(request);
+        if(!requestInJson.has("token")){
+            DAOUtils.sendResponse(response, new JSONObject().put("authenticated", false));
+        }else {
+            String idOfUser = getUserIdIfTokenIsValid(requestInJson.get("token").toString());
+            if(idOfUser.equals("invalidToken")) {
+                DAOUtils.sendResponse(response, new JSONObject().put("authenticated", false));
+            }else {
+                try {
+                    IndividualUser user = DAO.UserDAO.getIndividualUserById(idOfUser);
+                    servletRequest.setAttribute("user", user);
+                    servletRequest.setAttribute("credit", requestInJson.get("credit").toString());
+                    filterChain.doFilter(servletRequest, servletResponse);
+                } catch (Exception e) {
+                    DAOUtils.sendResponse(response, new JSONObject().put("serverError", true));
+                }
+            }
         }
     }
 
