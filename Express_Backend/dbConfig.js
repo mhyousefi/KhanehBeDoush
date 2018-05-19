71845afec6aa5a01bf261a085838759ea3055add
mhyousefi <mhyousefi96@gmail.com>
2018-05-20 00:03:10 +0430

    Modified dbConfig.js

--- Express_Backend/dbConfig.js
+++ Express_Backend/dbConfig.js
@@ -4,16 +4,10 @@ const HOME_DIR = require('os').homedir()
 const sequelizeDb = new Sequelize({
   host: 'localhost',
   dialect: 'sqlite',
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+  port: 8080,
 
   // SQLite only
-  storage: `${HOME_DIR}/Documents/IE_Project_Database`,
+  storage: `${HOME_DIR}/Documents/IE_Project_Database/khanehbedoush.db`,
 });
 
 
